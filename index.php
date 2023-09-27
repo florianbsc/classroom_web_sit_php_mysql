86767b6d820a70601b726d34f6d6cb75e96bae48
florianbsc <floflo_200068@hotmail.fr>
2023-09-27 11:20:06 +0200

    amelioration de l'affichage des recettes

--- index.php
+++ index.php
@@ -1,26 +1,51 @@
 <?php
 
-// Déclaration du tableau des recettes
 $recipes = [
-    ['Cassoulet', '[...]', 'mickael.andrieu@exemple.com', true,],
-    ['Couscous', '[...]', 'mickael.andrieu@exemple.com', false,],
+    [
+        'title' => 'Cassoulet',
+        'recipe' => 'Etape 1 : des flageolets !',
+        'author' => 'mickael.andrieu@exemple.com',
+        'is_enabled' => true,
+    ],
+    [
+        'title' => 'Couscous',
+        'recipe' => 'Etape 1 : de la semoule',
+        'author' => 'mickael.andrieu@exemple.com',
+        'is_enabled' => false,
+    ],
+    [
+        'title' => 'Escalope milanaise',
+        'recipe' => 'Etape 1 : prenez une belle escalope',
+        'author' => 'mathieu.nebra@exemple.com',
+        'is_enabled' => true,
+    ],
 ];
-
 ?>
-
 <!DOCTYPE html>
 <html>
-
 <head>
     <title>Affichage des recettes</title>
+    <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
+        rel="stylesheet"
+    >
 </head>
-
 <body>
-    <ul>
-        <?php for ($lines = 0; $lines <= 1; $lines++) : ?>
-            <li><?php echo $recipes[$lines][0] . ' (' . $recipes[$lines][2] . ')'; ?></li>
-        <?php endfor; ?>
-    </ul>
-</body>
+    <div class="container">
+        <h1>Affichage des recettes</h1>
+        <!-- Boucle sur les recettes -->
+        <?php foreach($recipes as $recipe) : ?>
+            <!-- si la clé existe et a pour valeur "vrai", on affiche -->
+            <?php if (array_key_exists('is_enabled', $recipe) && $recipe['is_enabled'] == true): ?>
 
+                <article>
+                    <h3><?php echo $recipe['title']; ?></h3>
+                    <div><?php echo $recipe['recipe']; ?></div>
+                    <i><?php echo $recipe['author']; ?></i>
+                </article>
+
+            <?php endif; ?>
+        <?php endforeach ?>
+    </div>   
+</body>
 </html>
