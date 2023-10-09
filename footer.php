a8b22c9625f4a26e5547b476c633e43080f00d1e
florianbsc <floflo_200068@hotmail.fr>
2023-10-09 16:19:53 +0200

    creation base du site avec les paage include, les vars et les fonctions.

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
-<nav id="footer">        
-    <div class="footer">
-        <h3>Footer</h3>
-        <p>Copyright moi, tous droits réservés</p>
-
-    </div>    
-</nav>
+<!-- footer.php -->
+<footer class="bg-light text-center text-lg-start mt-auto">
+  <div class="text-center p-3">
+    © 2021 Copyright:
+    <a class="text-dark" href="https://openclassrooms.com/">OpenClassrooms</a>
+  </div>
+</footer>
