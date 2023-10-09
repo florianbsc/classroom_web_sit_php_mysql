6715c1ec5715081641c54fc77fb6e35b37b653e0
florianbsc <floflo_200068@hotmail.fr>
2023-10-09 15:47:26 +0200

    creation des fichiers footer et header + intergration dans l'index

---
+++ footer.php
@@ -0,0 +1,7 @@
+<nav id="footer">        
+    <div class="footer">
+        <h3>Footer</h3>
+        <p>Copyright moi, tous droits réservés</p>
+
+    </div>    
+</nav>
--- index.php
+++ index.php
@@ -1,105 +1,33 @@
-<?php
-
-$users = [
-    [
-        'full_name' => 'Mickaël Andrieu',
-        'email' => 'mickael.andrieu@exemple.com',
-        'age' => 34,
-    ],
-    [
-        'full_name' => 'Mathieu Nebra',
-        'email' => 'mathieu.nebra@exemple.com',
-        'age' => 34,
-    ],
-    [
-        'full_name' => 'Laurène Castor',
-        'email' => 'laurene.castor@exemple.com',
-        'age' => 28,
-    ],
-];
-
-$recipes = [
-    [
-        'title' => 'Cassoulet',
-        'recipe' => '',
-        'author' => 'mickael.andrieu@exemple.com',
-        'is_enabled' => true,
-    ],
-    [
-        'title' => 'Couscous',
-        'recipe' => '',
-        'author' => 'mickael.andrieu@exemple.com',
-        'is_enabled' => false,
-    ],
-    [
-        'title' => 'Escalope milanaise',
-        'recipe' => '',
-        'author' => 'mathieu.nebra@exemple.com',
-        'is_enabled' => true,
-    ],
-    [
-        'title' => 'Salade Romaine',
-        'recipe' => '',
-        'author' => 'laurene.castor@exemple.com',
-        'is_enabled' => false,
-    ],
-];
-
-function displayAuthor(string $authorEmail, array $users) : string
-{
-    for ($i = 0; $i < count($users); $i++) {
-        $author = $users[$i];
-        if ($authorEmail === $author['email']) {
-            return $author['full_name'] . '(' . $author['age'] . ' ans)';
-        }
-    }
-}
-
-function isValidRecipe(array $recipe) : bool
-{
-    if (array_key_exists('is_enabled', $recipe)) {
-        $isEnabled = $recipe['is_enabled'];
-    } else {
-        $isEnabled = false;
-    }
-
-    return $isEnabled;
-}
-
-function getRecipes(array $recipes) : array
-{
-    $valid_recipes = [];
-
-    foreach($recipes as $recipe) {
-        if (isValidRecipe($recipe)) {
-            $valid_recipes[] = $recipe;
-        }
-    }
-
-    return $valid_recipes;
-}
-
-?>
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>Recettes de cuisine</title>
-    <link
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
-        rel="stylesheet"
-    >
-</head>
-<body>
-    <div class="container">
-        <h1>Liste des recettes de cuisine</h1>
-
-        <?php foreach(getRecipes($recipes) as $recipe) : ?>
-            <article>
-                <h3><?php echo $recipe['title']; ?></h3>
-                <div><?php echo $recipe['recipe']; ?></div>
-                <i><?php echo displayAuthor($recipe['author'], $users); ?></i>
-            </article>
-        <?php endforeach ?>
-    </div>   
-</body>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Mon super site</title>
+    </head>
+ 
+    <body>
+ 
+    <!-- L'en-tête -->
+    
+        <!-- Le menu -->
+        <?php include('header.php'); ?>
+       
+    
+    <!-- Le corps -->
+    
+    <div id="corps">
+        <h1>Mon super site</h1>
+        
+        <p>
+            Bienvenue sur mon super site !<br />
+            Vous allez adorer ici, c'est un site génial qui va parler de... euh... Je cherche encore un peu le thème de mon site. :-D
+        </p>
+    </div>
+    
+    <!-- Le pied de page -->
+    
+    <?php include('footer.php'); ?>
+    
+    
+    </body>
 </html>
